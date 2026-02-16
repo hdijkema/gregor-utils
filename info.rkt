e0949c67ef4c2476dd9f4848891dc64e93c8db56
Hans Dijkema <hans@dijkewijk.nl>
2026-02-16 10:44:02 +0100

    version update

--- info.rkt
+++ info.rkt
@@ -1,7 +1,7 @@
 #lang info
 
 (define pkg-authors '(hnmdijkema))
-(define version "0.1.1")
+(define version "0.1.2")
 (define license 'Apache-2.0)
 (define pkg-desc "A few utility functions around the gregor date module")
 
